dd274d70073522eca0e686de54794cd60713538f
Shale Ahammed Taspy <shaleahamedtaspi@gmail.com>
2025-06-22 18:16:25 +0600

    new added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,28 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+
+
+        
+<div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+
+
+        <p className='p-20'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
